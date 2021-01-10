551b8da9d4c8f5ab91ab295ad0e50f723693309f
Ariadne Conill <ariadne@dereferenced.org>
2021-01-10 02:05:56 -0700

    logging: use DEBUG loglevel if JEJUNE_DEBUG is set in environ

--- jejune/logging.py
+++ jejune/logging.py
@@ -1,8 +1,14 @@
 import logging
+import os
+
+
+level = logging.INFO
+if 'JEJUNE_DEBUG' in os.environ:
+    level = logging.DEBUG
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=level,
     format="[%(asctime)s] %(levelname)s: %(message)s",
     handlers=[logging.StreamHandler()]
 )
