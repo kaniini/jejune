03a4d6fba23383cd650190bc1192074f254b7dd1
Ariadne Conill <ariadne@dereferenced.org>
2021-01-10 07:07:33 -0700

    add nodeinfo

--- jejune/web/__init__.py
+++ jejune/web/__init__.py
@@ -1,5 +1,6 @@
 from . import object
 from . import webfinger
+from . import nodeinfo
 from . import mastodon_api
 from . import pleroma_api
 from . import static
---
+++ jejune/web/nodeinfo.py
@@ -0,0 +1,46 @@
+from .. import app, __version__
+from aiohttp.web import RouteTableDef, json_response
+
+
+routes = RouteTableDef()
+
+
+@routes.get('/.well-known/nodeinfo')
+def nodeinfo_links(request):
+    links = [
+        {'rel': 'http://nodeinfo.diaspora.software/ns/schema/2.0',
+         'href': 'https://{0}/.well-known/nodeinfo/2.0.json'.format(app.config['instance']['hostname'])},
+        {'rel': 'http://nodeinfo.diaspora.software/ns/schema/2.1',
+         'href': 'https://{0}/.well-known/nodeinfo/2.1.json'.format(app.config['instance']['hostname'])},
+    ]
+    return json_response({'links': links})
+
+
+# XXX: would be nice to get Pleroma FE to look up the actual URIs.
+@routes.get('/.well-known/nodeinfo/{version}.json')
+@routes.get('/nodeinfo/{version}.json')
+def nodeinfo_response(request):
+    data = {
+        'metadata': {
+             'nodeDescription': app.config['instance'].get('description'),
+             'nodeName': app.config['instance'].get('name'),
+        },
+        'openRegistrations': app.config['instance'].get('registrations', False),
+        'protocols': [
+             'activitypub',
+        ],
+        'services': {
+             'inbound': [],
+             'outbound': [],
+        },
+        'software': {
+             'name': 'jejune',
+             'repository': 'https://github.com/kaniini/jejune',
+             'version': __version__,
+        },
+        'version': request.match_info['version'],
+    }
+    return json_response(data)
+
+
+app.add_routes(routes)
