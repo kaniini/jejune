b7078717b9cbd27f3862c4e79fac5af87af3c88d
Ariadne Conill <ariadne@dereferenced.org>
2021-01-10 04:21:41 -0700

    user: add User.verify_password() and User.new()

--- jejune/user.py
+++ jejune/user.py
@@ -1,5 +1,17 @@
+import crypt
+
+
 from .serializable import Serializable
 
 
 class User(Serializable):
-    pass
+    @classmethod
+    def new(cls, app, username: str, email: str, password: str) -> Serializable:
+        crypted_pass = crypt.crypt(password, crypt.mksalt(crypt.METHOD_BLOWFISH))
+        actor_uri = app.rdf_object_uri()
+
+        return cls(username=username, email=email, password=crypted_pass, actor_uri=actor_uri)
+
+    def verify_password(self, password: str) -> bool:
+        crypted_pass = crypt.crypt(password, self.password)
+        return self.password == crypted_pass
