4b1e78cddfdc416c3dd11e02d266661ab9fd9eeb
Ariadne Conill <ariadne@dereferenced.org>
2021-01-10 01:51:40 -0700

    gracefully error if JEJUNE_CONFIG is not set

--- jejune/__init__.py
+++ jejune/__init__.py
@@ -10,4 +10,9 @@ import os
 from . import application_factory
 
 
+if 'JEJUNE_CONFIG' not in os.environ:
+    print('JEJUNE_CONFIG is not in environment, cannot load application')
+    exit()
+
+
 app = application_factory.Application(os.environ['JEJUNE_CONFIG'])
