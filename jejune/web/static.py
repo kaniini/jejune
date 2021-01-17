dc78dc9fcf518ebf28395aabca2b588250a71fec
Ariadne Conill <ariadne@dereferenced.org>
2021-01-17 08:50:36 -0700

    static: move bundled client to /.well-known/jejune

--- jejune/web/static.py
+++ jejune/web/static.py
@@ -7,6 +7,6 @@ async def index_html(request):
     return FileResponse(path=app.config['paths']['static'] + '/index.html')
 
 
-app.router.add_get('/', index_html)
+app.router.add_get('/.well-known/jejune', index_html)
 app.router.add_static('/static', path=app.config['paths']['static'] + '/static', name='static')
 app.router.add_static('/.well-known/jejune/upload', path=app.config['paths']['upload'], name='upload')
