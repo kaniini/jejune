199e3689aeaf78f4b2a3c44066d0dc0f6c85a03b
Ariadne Conill <ariadne@dereferenced.org>
2021-01-16 19:07:14 -0700

    implement accept/reject verbs

--- jejune/activity_pub/verbs.py
+++ jejune/activity_pub/verbs.py
@@ -1,3 +1,6 @@
+import logging
+
+
 from ..activity_streams import AS2Activity, registry
 
 
@@ -26,3 +29,37 @@ class Update(AS2Activity):
         object.commit()
 
 registry.register_type(Update)
+
+
+class Accept(AS2Activity):
+    __jsonld_type__ = 'Accept'
+    __ephemeral__ = True
+
+    async def apply_side_effects(self):
+        object = self.child()
+
+        if not isinstance(object, AS2Activity):
+            logging.info('WTF: Accept Activity %r references child object %r (%s) which does not have applicable side effects.',
+                         self.id, object.id, object.type)
+            return
+
+        await object.accept_side_effects()
+
+registry.register_type(Accept)
+
+
+class Reject(AS2Activity):
+    __jsonld_type__ = 'Reject'
+    __ephemeral__ = True
+
+    async def apply_side_effects(self):
+        object = self.child()
+
+        if not isinstance(object, AS2Activity):
+            logging.info('WTF: Reject Activity %r references child object %r (%s) which does not have applicable side effects.',
+                         self.id, object.id, object.type)
+            return
+
+        await object.reject_side_effects()
+
+registry.register_type(Reject)
