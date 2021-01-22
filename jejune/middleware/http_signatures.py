a372c72959978b4fb431ca67fd92a2c4da40d13d
Ariadne Conill <ariadne@dereferenced.org>
2021-01-22 15:25:24 +0000

    middleware: http signatures: make the validator less chatty

--- jejune/middleware/http_signatures.py
+++ jejune/middleware/http_signatures.py
@@ -15,11 +15,11 @@ async def http_signatures_middleware(app, handler):
             data = await request.json()
 
             if 'actor' not in data:
-                raise aiohttp.web.HTTPUnauthorized(body='signature check failed, no actor in message')
+                return aiohttp.web.json_response({'error': 'signature check failed, no actor in message'}, status=403)
 
             if not (await verifier.validate(data['actor'], request)):
                 logging.info('HTTP Signature validation failed for: %s', data['actor'])
-                raise aiohttp.web.HTTPUnauthorized(body='signature check failed, signature did not match key')
+                return aiohttp.web.json_response({'error': 'signature check failed, signature did not match key'}, status=403)
 
         return (await handler(request))
 
