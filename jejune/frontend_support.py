b99de2e5fb70e6e07f56518b530df675dd1818f8
Ariadne Conill <ariadne@dereferenced.org>
2021-01-25 05:34:28 +0000

    frontend support: if a friendly url already exists, provide it

--- jejune/frontend_support.py
+++ jejune/frontend_support.py
@@ -3,4 +3,7 @@ from .activity_streams import AS2Object
 
 class FrontendSupport:
     def friendly_uri(self, object: AS2Object) -> str:
+        if object.url:
+            return object.url
+
         return object.id
