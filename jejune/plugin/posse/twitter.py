68588c6f40f2f35673c66dd01245371a57bd8028
Ariadne Conill <ariadne@dereferenced.org>
2021-01-29 05:00:59 +0000

    posse to twitter: implement POSSE of text-only posts

--- jejune/plugin/posse/twitter.py
+++ jejune/plugin/posse/twitter.py
@@ -2,6 +2,8 @@ import logging
 
 
 from jejune import app
+from jejune.activity_streams import AS2Object
+from peony import PeonyClient
 
 
 def get_twitter_config() -> dict:
@@ -17,7 +19,65 @@ def preflight() -> bool:
     return False not in present
 
 
+client = None
+
+
+def stemmer(msg):
+    chopped = msg[0:240]
+    if chopped[-1] in {'.', '?', '!'}:
+        return chopped
+
+    chopped, _ = chopped.rsplit(None, 1)
+    return ''.join([chopped, '…'])
+
+
+async def listener(uri: str, activity: AS2Object):
+    logging.info('POSSE to Twitter: Listener was called!')
+
+    if activity.type != 'Create':
+        return
+
+    child = activity.child()
+    if not child:
+        return
+
+    # get the markdown source
+    source = getattr(child, 'summary', None)
+    if not source:
+        source = getattr(child, 'source', {}).get('content', None)
+    if not source:
+        return
+
+    logging.debug('POSSE to Twitter: Attempting to POSSE object %r to Twitter [%s].', child.id, source)
+
+    # trim the source text down so it will fit in a tweet (240 chars, allowing 40 for the shortlink)
+    stem = stemmer(source)
+    final_status = ' '.join([stem, child.url])
+
+    result = await client.api.statuses.update.post(status=final_status)
+    logging.debug('POSSE to Twitter: got %r from API', result)
+
+
 if preflight():
-    pass
+    cfg = get_twitter_config()
+
+    client = PeonyClient(consumer_key=cfg['consumer-key'],
+                         consumer_secret=cfg['consumer-secret'],
+                         access_token=cfg['access-token'],
+                         access_token_secret=cfg['access-secret'])
+
+    for user in cfg['users']:
+        u = app.userapi.find_user(user)
+        if not u:
+            logging.info('POSSE to Twitter: Attempting to POSSE for non-existent account %r.', user)
+            continue
+
+        a = u.actor()
+        if not a:
+            logging.info('POSSE to Twitter: Attempting to POSSE for non-existent actor belonging to account %r.', user)
+            continue
+
+        logging.info('POSSE to Twitter: Setting up listener for outbox %r.', a.outbox)
+        app.publisher.attach_uri_listener(a.outbox, listener)
 else:
     logging.info('POSSE to Twitter: Preflight checks failed; make sure valid consumer and access tokens and secrets are configured!')
