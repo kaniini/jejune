3eb69e565dfafc9e54b48c7fb0a04aeae66234e9
Ariadne Conill <ariadne@dereferenced.org>
2021-01-29 04:07:26 +0000

    posse: add twitter plugin skeleton

---
+++ jejune/plugin/posse/twitter.py
@@ -0,0 +1,23 @@
+import logging
+
+
+from jejune import app
+
+
+def get_twitter_config() -> dict:
+    return app.config.get('raw', {}).get('twitter', {})
+
+
+def preflight() -> bool:
+    cfg = get_twitter_config()
+    if not cfg:
+        return False
+    required_keys = ['consumer-key', 'consumer-secret', 'access-token', 'access-secret']
+    present = [k in cfg.keys() for k in required_keys]
+    return False not in present
+
+
+if preflight():
+    pass
+else:
+    logging.info('POSSE to Twitter: Preflight checks failed; make sure valid consumer and access tokens and secrets are configured!')
