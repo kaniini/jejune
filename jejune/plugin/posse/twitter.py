9229234dbfed45473b8800132cf43f141286cf0f
Ariadne Conill <ariadne@dereferenced.org>
2021-01-29 04:19:16 +0000

    posse: twitter: add user mapping

--- jejune/plugin/posse/twitter.py
+++ jejune/plugin/posse/twitter.py
@@ -12,7 +12,7 @@ def preflight() -> bool:
     cfg = get_twitter_config()
     if not cfg:
         return False
-    required_keys = ['consumer-key', 'consumer-secret', 'access-token', 'access-secret']
+    required_keys = ['consumer-key', 'consumer-secret', 'access-token', 'access-secret', 'users']
     present = [k in cfg.keys() for k in required_keys]
     return False not in present
 
