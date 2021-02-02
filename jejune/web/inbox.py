9e72e8fa92b3050ac9f672c8fdb7648da41abdc6
Ariadne Conill <ariadne@dereferenced.org>
2021-02-02 04:57:13 +0000

    activity pub: wire up C2S outbox POST endpoint

--- jejune/web/inbox.py
+++ jejune/web/inbox.py
@@ -69,4 +69,35 @@ async def outbox_get(request):
     return json_response(adapter.serialize(request))
 
 
+@routes.post('/.well-known/jejune/outbox/{id}')
+async def outbox_post(request):
+    outbox_id = request.match_info.get('id', None)
+
+    if not outbox_id:
+        return json_response({'status': 'invalid capability URI'}, status=403)
+
+    u = request.get('oauth_user', None)
+    if not u:
+        return json_response({'status': 'unauthorized; matching oauth session token needed to read personal inboxes'}, status=403)
+
+    a = u.actor()
+    if not a:
+        return json_response({'status': 'unauthorized; oauth session token is associated with non-existent actor'}, status=403)
+
+    if not a.inbox.endswith(outbox_id):
+        return json_response({'status': 'outbox capability URI does not match session token'}, status=403)
+
+    try:
+        payload = await request.json()
+
+        if not resp:
+            return json_response({'status': 'rejected; malformed payload'}, status=406)
+
+        app.inbox_processor.enqueue(resp)
+    except:
+        return json_response({'status': 'rejected; malformed payload'}, status=406)
+
+    return json_response({'status': 'accepted'}, status=202)
+
+
 app.add_routes(routes)
