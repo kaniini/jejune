5de27584ceb04cbf034d0a5c55dd05249e831b28
Ariadne Conill <ariadne@dereferenced.org>
2021-02-13 06:21:48 +0000

    web: object: add route for instance actor (closes #4)

--- jejune/web/object.py
+++ jejune/web/object.py
@@ -15,4 +15,14 @@ async def object(request):
      return Response(text=object, content_type='application/activity+json')
 
 
+@routes.get('/.well-known/jejune/actor')
+async def instance_actor(request):
+     uri = app.make_well_known_uri('actor')
+     if not app.rdf_store.local_uri_exists(uri):
+         return json_response({'error': 'object not found'}, status=404)
+
+     (object, mtime) = app.rdf_store.fetch_cached(uri)
+     return Response(text=object, content_type='application/activity+json')
+
+
 app.add_routes(routes)
